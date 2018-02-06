e50b018bf2cd8b1946ab868a2e57223721d36c0d
Samuel Bering <samuelbering@hotmail.com>
2018-02-06 16:20:31 +0100

    Added serverside validation for post booking

--- ManageBookings/manageBookings.js
+++ ManageBookings/manageBookings.js
@@ -1,6 +1,7 @@
 const firebase = require('firebase');
 const BookingManager = require("./bookingManager.js");
 const Booking = require("./booking.js");
+const validator = require('validator');
 
 let config = {
     apiKey: "AIzaSyC6cL89hWY3EYQz5V2rScWyLEGpWTXhi7Q",
@@ -25,7 +26,104 @@ exports.getUnavailableDates = (req, res) => {
 
 };
 
+
+function validateDate(date, errorStrings) {
+
+    let isDate = validator.matches(date, /^\d{4}-\d{2}-\d{2}$/);
+    if (!isDate)
+        errorStrings.push(`Ej korrekt datumformat: ${date}`);
+}
+
+
+function validateDates(dates, errorStrings) {
+
+    if (dates == null || dates.length == 0) {
+        errorStrings.push(`Minst ett datum är obligatoriskt`);
+        return;
+    }
+
+    for (let date of dates) {
+        validateDate(date, errorStrings);
+    }
+
+}
+
+function hasValue(name, value, errorStrings) {
+    if (value == null || value == "") {
+        errorStrings.push(`${name} är obligatoriskt`);
+        return false;
+    }
+
+    return true;
+}
+
+function validateCurrency(currency, errorStrings) {
+
+    if (!hasValue("Belopp", currency, errorStrings))
+        return;
+
+    let isCurrency = validator.isCurrency(currency);
+
+    if (!isCurrency)
+        errorStrings.push(`Ej korrekt angivet belopp: ${currency}`);
+
+}
+
+function validateIdentityNumber(identityNumber, errorStrings) {
+
+    if (!hasValue("Personnummer", identityNumber, errorStrings))
+        return;
+
+    let isIdentityNumber = validator.matches(identityNumber, /^\d{6}-\d{4}$|^\d{8}-\d{4}$/);
+
+    if (!isIdentityNumber)
+        errorStrings.push(`Ej korrekt personnummer: ${identityNumber}`);
+
+}
+
+function validateEmail(email, errorStrings) {
+
+    if (!hasValue("Email", email, errorStrings))
+        return;
+
+    let isEmail = validator.isEmail(email);
+
+    if (!isEmail)
+        errorStrings.push(`Ej korrekt email: ${email}`);
+
+}
+
 exports.makeBooking = (req, res) => {
+
+    let errorStrings = [];
+
+    validateDates(req.body.dates, errorStrings);
+
+    validateCurrency(req.body.toPay + '', errorStrings);
+
+    validateIdentityNumber(req.body.personalIdentityNumber, errorStrings);
+
+    hasValue("Förnamn", req.body.firstName, errorStrings);
+
+    hasValue("Efternamn", req.body.lastName, errorStrings);
+
+    hasValue("Gatuadress", req.body.streetAddress, errorStrings);
+
+    hasValue("Postnummer", req.body.zipCode, errorStrings);
+
+    hasValue("Stad", req.body.postTown, errorStrings);
+
+    hasValue("Telefonnummer", req.body.phoneNumber, errorStrings);
+
+    validateDate(req.body.bookingDate, errorStrings);
+
+    validateEmail(req.body.email, errorStrings);
+
+    if (errorStrings.length > 0) {
+        res.status(400).json({ validationErrors: errorStrings, status: "Kunde ej validera postad data" });
+        return;
+    }
+
 
     let booking = new Booking(req.body.dates, req.body.toPay, req.body.firstName,
         req.body.lastName, req.body.personalIdentityNumber,
@@ -34,6 +132,6 @@ exports.makeBooking = (req, res) => {
 
     bookingManager.MakeBooking(booking);
 
-    res.status(200).json({status:"ok"})
+    res.status(200).json({ status: "ok" })
 
 };
